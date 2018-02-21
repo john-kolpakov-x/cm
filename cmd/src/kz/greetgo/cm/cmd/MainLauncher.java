181998321775bf3bb58089841ee40850c5d9a496
pompei <john.kolpakov.x@gmail.com>
2018-02-21 23:41:04 +0600

    Придумана структура команды запуска

--- cmd/src/kz/greetgo/cm/cmd/MainLauncher.java
+++ cmd/src/kz/greetgo/cm/cmd/MainLauncher.java
@@ -1,7 +1,27 @@
 package kz.greetgo.cm.cmd;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class MainLauncher {
-  public static void main(String[] args) {
-    System.out.println("Hello from JAVA");
+  public static void main(String[] args) throws Exception {
+    int totalCount = 0;
+
+    if (System.in.available() > 0) {
+      InputStream in = System.in;
+      PrintStream out = System.out;
+      byte buf[] = new byte[1024 * 4];
+
+      while (true) {
+        int count = in.read(buf);
+        if (count < 0) break;
+        totalCount += count;
+        out.write(buf, 0, count);
+      }
+
+    }
+    System.out.println("Hello from MainLauncher: totalCount = " + totalCount);
+
+//    System.out.println("hi args.length = " + args.length);
   }
 }
--- scripts/oo.sh
+++ scripts/oo.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-SCRIPT_DIR=$(dirname $(readlink -f $0))
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 
-GRADLE=${SCRIPT_DIR}/../gradlew
+if [ "update" = "$1" ] ; then
 
-echo GRADLE = $GRADLE
+  if [ "skip-git-pull" = "$2" ] ; then
+    echo skip git pull
+  else
+    cd ${SCRIPT_DIR}/..
+    git pull
+  fi
+
+  GRADLE=$(realpath ${SCRIPT_DIR}/../gradlew)
+
+  cd ${SCRIPT_DIR}/../cmd
+  rm -rf build
+
+  ${GRADLE} cmdFileCreator
+
+  exit 0
+fi
+
+RUN_CMD=${SCRIPT_DIR}/../cmd/build/cmd/run.cmd.txt
+
+exec $(cat ${RUN_CMD}) "$@"
